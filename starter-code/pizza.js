6908238592e6d17ab469003745aafd04bcb28de3
DdSG <daniel@hubrix.co>
2018-10-09 21:10:35 +0200

    Refactored code to use array of classnames

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,19 +1,30 @@
 // Write your Pizza Builder JavaScript in this file.
 
+/*
+Simple array of pizza toppings/options.
+btn: (string) CSS selector of topping button
+price: (number) price of that topping or option
+dc: (string) CSS selector of topping shown on "demo" pizza
+psel: (string) CSS selector of topping cost in price display
+togc: (optional, string) CSS selector of class to toggle (for sauce, crust)
+*/
+var pizzaOptions = [
+    {btn: '.btn-pepperonni', price: 1, dc: '.pep', psel: '.price > ul li:nth-child(1)' }
+    , {btn: '.btn-mushrooms', price: 1, dc: '.mushroom', psel: '.price > ul li:nth-child(2)' }
+    , {btn: '.btn-green-peppers', price: 1, dc: '.green-pepper', psel: '.price > ul li:nth-child(3)' }
+    , {btn: '.btn-sauce', price: 3, dc: '.sauce', psel: '.price > ul li:nth-child(4)',
+        togc: 'sauce-white'}
+    , {btn: '.btn-crust', price: 5, dc: '.crust', psel: '.price > ul li:nth-child(5)',
+        togc: 'crust-gluten-free' }
+];
+
 // Helper function to update prices
 function updatePrice() {
-    const hasPepperonni = $('.btn-pepperonni').hasClass('active');
-    const hasMushrooms = $('.btn-mushrooms').hasClass('active');
-    const hasGreenPepper = $('.btn-green-peppers').hasClass('active');
-    const hasWhiteSauce = $('.btn-sauce').hasClass('active');
-    const hasGlutenFreeCrust = $('.btn-crust').hasClass('active');
-
-    let price = 10;
-    price += hasPepperonni ? 1 : 0;
-    price += hasMushrooms ? 1 : 0;
-    price += hasGreenPepper ? 1 : 0;
-    price += hasWhiteSauce ? 3 : 0;
-    price += hasGlutenFreeCrust ? 5 : 0;
+    var price = 10;
+    for (var i = 0; i < pizzaOptions.length; i++) {
+        var hasOption = $(pizzaOptions[i].btn).hasClass('active');
+        price += hasOption ? pizzaOptions[i].price : 0;
+    }
 
     $('.price > strong').text('$' + price);
 }
@@ -38,12 +49,6 @@ function configTopping(btn,democlass,pricesel,togclass = undefined) {
 }
 
 $(document).ready(function() {
-    configTopping('.btn-pepperonni','.pep','.price > ul li:nth-child(1)');
-    configTopping('.btn-mushrooms','.mushroom','.price > ul li:nth-child(2)');
-    configTopping('.btn-green-peppers','.green-pepper','.price > ul li:nth-child(3)');
-    configTopping('.btn-sauce','.sauce','.price > ul li:nth-child(4)','sauce-white');
-    configTopping('.btn-crust','.crust','.price > ul li:nth-child(5)','crust-gluten-free');
-    
     /*
         Create default pizza as specified for Iteration 2:
 
@@ -52,15 +57,24 @@ $(document).ready(function() {
         will let users select white sauce and gluten-
         free crust if they want to choose them.""
     */ 
-    $('.crust').toggleClass('crust-gluten-free');
-    $('.sauce').toggleClass('sauce-white');
-    $('.price > ul li:nth-child(4)').toggle();
-    $('.price > ul li:nth-child(5)').toggle();
-    
-    if(!$('.pep').is(':visible')) $('.btn-pepperonni').removeClass('active');
-    if(!$('.mushroom').is(':visible')) $('.btn-mushrooms').removeClass('active');
-    if(!$('.green-pepper').is(':visible')) $('.btn-green-peppers').removeClass('active');
-    if(!$('.crust').hasClass('crust-gluten-free')) $('.btn-crust').removeClass('active');
-    if(!$('.sauce').hasClass('sauce-white')) $('.btn-sauce').removeClass('active');
+   $('.crust').toggleClass('crust-gluten-free');
+   $('.sauce').toggleClass('sauce-white');
+   $('.price > ul li:nth-child(4)').toggle();
+   $('.price > ul li:nth-child(5)').toggle();
 
+    // Set up all our click functions and initialize toppings etc. status
+   for (var i = 0; i < pizzaOptions.length; i++) {
+        configTopping(pizzaOptions[i].btn,pizzaOptions[i].dc,
+            pizzaOptions[i].psel,pizzaOptions[i].togc); // declares + binds "click" function
+        if (undefined === pizzaOptions[i].togc) {
+            if(!$(pizzaOptions[i].dc).is(':visible'))
+            $(pizzaOptions[i].btn).removeClass('active');
+        }
+        else {
+            if(!$(pizzaOptions[i].dc).hasClass(pizzaOptions[i].togc))
+                $(pizzaOptions[i].btn).removeClass('active');
+        }
+    }
+
+    updatePrice(); // ensure initial price displayed is correct
 });
